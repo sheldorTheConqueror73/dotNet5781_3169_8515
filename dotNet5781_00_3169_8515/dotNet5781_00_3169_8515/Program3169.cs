4e7f0313376b11a7c6c261c9e0c4fc311c5d03cb
itamaroh7 <itamaroh7@gmail.com>
2020-10-14 22:28:49 +0300

    new class +Welcome 3169()

---
+++ dotNet5781_00_3169_8515/dotNet5781_00_3169_8515/Program3169.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_3169_8515
+{
+    partial class Program
+    {
+      static partial void  Welcome3169()
+       {
+            Console.WriteLine("I am also here ");
+       }
+    }
+}
