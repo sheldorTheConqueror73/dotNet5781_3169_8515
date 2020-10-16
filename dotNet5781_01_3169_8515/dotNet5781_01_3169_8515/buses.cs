71081e295cc023e4aecf4ee013726165d3b5c6a5
sheldorTheConqueror73 <yonatan.y.tamir@gmail.com>
2020-10-16 09:53:43 +0300

    buses class init

---
+++ dotNet5781_01_3169_8515/dotNet5781_01_3169_8515/buses.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_01_3169_8515
+{
+    struct DateTime
+    {
+        int day, month, year;
+       
+    }
+
+    partial class buses
+    {
+
+        int id, fuel, distance;
+        bool Dangerous;
+        DateTime startDate;
+
+        public buses()
+        {
+            id = -1;
+            fuel = 0;
+            distance = 0;
+            Dangerous = false;
+            startDate.day = 0;
+            startDate.month = 0;
+            startDate.year = 0;
+        }
+    }
+    partial class buses
+    {
+
+    }
+}
