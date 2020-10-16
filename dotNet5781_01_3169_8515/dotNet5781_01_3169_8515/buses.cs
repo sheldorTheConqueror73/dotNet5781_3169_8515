acd211ddaf0cf974cfd484a2443c95f22c6c4c58
itamaroh7 <itamaroh7@gmail.com>
2020-10-16 10:45:47 +0300

    BusDangerous()+getters/setters in Date struct

--- dotNet5781_01_3169_8515/dotNet5781_01_3169_8515/buses.cs
+++ dotNet5781_01_3169_8515/dotNet5781_01_3169_8515/buses.cs
@@ -1,31 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace dotNet5781_01_3169_8515
 {
-    public struct DateTime
+    public struct DateTimes
     {
         public int day;
         public int month;
         public int year;
 
-        public void Date()
+        public DateTimes(int sum=0) 
         {
+
+                Console.WriteLine("enter day: ");
+                string input = Console.ReadLine();
+                Int32.TryParse(input,out  day);
+                //if (day < 1 || day > 31)
+               
+               
+                Console.WriteLine("enter month: ");
+                input = Console.ReadLine();
+                Int32.TryParse(input, out month);
+                //if(month<1||month>12)
+
+                Console.WriteLine("enter year: ");
+                input = Console.ReadLine();
+                Int32.TryParse(input, out year);
+               // if(year<2000||year>2020)
            
-            Console.WriteLine("enter day: ");
-            string input = Console.ReadLine();
-            Int32.TryParse(input, out day);
-            Console.WriteLine("enter month: ");
-            input = Console.ReadLine();
-            Int32.TryParse(input, out month);
-            Console.WriteLine("enter year: ");
-            input = Console.ReadLine();
-            Int32.TryParse(input, out year);
+            
 
         }
+        public int GetDay() { return this.day; }
+        public int GetMonth() { return this.month; }
+        public int GetYear() { return this.year; }
+        public void SetDay(int _day) { this.day = _day; }
+        public void SetMonth(int _month) { this.month = _month; }
+        public void SetYear(int _year) { this.year = _year; }
+
+
 
     }
 
@@ -34,7 +51,7 @@ namespace dotNet5781_01_3169_8515
 
         int id, fuel, distance;
         bool Dangerous;
-        DateTime startDate;
+        DateTimes startDate;
 
         public buses()
         {
@@ -45,6 +62,17 @@ namespace dotNet5781_01_3169_8515
             startDate.day = 0;
             startDate.month = 0;
             startDate.year = 0;
+        }
+
+       public void Maintenance()
+        {
+            
+        }
+        public void BusDangerous()
+        {
+            if (distance > 20000)
+                Dangerous = true;
+
         }
     }
     partial class buses
