2c02b3aea5074a82190db3a2935b67e83b6d3e5a
sheldorTheConqueror73 <yonatan.y.tamir@gmail.com>
2020-10-29 19:35:25 +0200

    added

--- dotNet5781_3169_8515/dotNet5781_02_3169_8515/busLineStation.cs
+++ dotNet5781_3169_8515/dotNet5781_02_3169_8515/busLineStation.cs
@@ -8,6 +8,17 @@ namespace dotNet5781_02_3169_8515
 {
     class busLineStation : busStation
     {
+        private int distance;//maybe add readonly?
+        private TimeSpan driveTime;
 
+        internal busLineStation()
+        {
+            distance = -1;
+            driveTime= new TimeSpan();
+        }
+        internal busLineStation(string id, int lat, int lon, string address= "")
+        {
+
+        }
     }
 }
