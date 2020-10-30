4e73e41fe300ed9a43eebc598ae544df3b563e62
itamaroh7 <itamaroh7@gmail.com>
2020-10-30 12:19:31 +0200

    get and set properties of Busline class

--- dotNet5781_3169_8515/dotNet5781_02_3169_8515/busLine.cs
+++ dotNet5781_3169_8515/dotNet5781_02_3169_8515/busLine.cs
@@ -7,17 +7,58 @@ using System.Threading.Tasks;
 namespace dotNet5781_02_3169_8515
 {
     class busLine
-    {
-        enum Areas   {General,North,South,Center,Jerusalem,JurdenVally }
+    { 
         List<busLineStation> path = new List<busLineStation>();
-
         private readonly string id;
         private string firstStation, lastStation;
         Areas area;
-
-        public busLine()
+        public busLine(List<busLineStation> _path,string _id, string _firstStation, string _lastStation)
+        {
+            this.path = _path;
+            this.id = _id;
+            this.firstStation = _firstStation;
+            this.lastStation = _lastStation;
+        }
+        internal string Id
+        {
+            get => id;
+        }
+        internal string FirstStation
+        {
+            get => firstStation;
+            set
+            {
+                firstStation = value;
+            }
+        }
+        internal string LastStation
+        {
+            get => lastStation;
+            set
+            {
+                lastStation = value;
+            }
+        }
+        internal Areas Area
+        {
+            get => area;
+            set
+            {
+                area = value;
+            }
+        }
+        internal List<busLineStation> Path
+        {
+            get => path;
+            set
+            {
+                path = value;
+            }
+        }
+        public override string ToString()
         {
             
         }
+
     }
 }
