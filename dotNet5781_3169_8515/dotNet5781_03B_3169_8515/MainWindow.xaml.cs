bae2a738e11b6dff701223d1531edb95ca86331d
sheldorTheConqueror73 <yonatan.y.tamir@gmail.com>
2020-11-24 12:29:52 +0200

    more rsc

--- dotNet5781_3169_8515/dotNet5781_03B_3169_8515/MainWindow.xaml.cs
+++ dotNet5781_3169_8515/dotNet5781_03B_3169_8515/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using dotNet5781_01_3169_8515;
 
 namespace dotNet5781_03B_3169_8515
 {
@@ -20,9 +21,26 @@ namespace dotNet5781_03B_3169_8515
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static List<buses> busPool;
+        Random r = new Random();
+        string appPath = AppDomain.CurrentDomain.BaseDirectory + "..\\..\\";
         public MainWindow()
         {
             InitializeComponent();
+            initBus();
+        }
+        public void initBus()
+        {
+            int month, day, year;
+            year = r.Next(1980, DateTime.Now.Year + 1);
+            if(year=DateTime.Now.Year)
+
+            DateTime rd = new DateTime(r.Next(1980,DateTime.Now.Year+1),)
+            for (int i = 0; i < 13; i++)
+            {
+                busPool[i] = new buses();
+            }
         }
     }
+
 }
