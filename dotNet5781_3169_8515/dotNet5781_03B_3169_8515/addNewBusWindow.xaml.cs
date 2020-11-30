a6a55c59e63e72f0b59e8f8fe790aaf4eaf40306
sheldorTheConqueror73 <yonatan.y.tamir@gmail.com>
2020-11-30 20:47:10 +0200

    oops

--- dotNet5781_3169_8515/dotNet5781_03B_3169_8515/addNewBusWindow.xaml.cs
+++ dotNet5781_3169_8515/dotNet5781_03B_3169_8515/addNewBusWindow.xaml.cs
@@ -74,7 +74,7 @@ namespace dotNet5781_03B_3169_8515
                 txbTotalDistance.IsEnabled = true;
                 return;
             }
-            buses bs1 = new buses((DateTime)dpRegiDate.SelectedDate, (DateTime)dplmiDate.SelectedDate, txbid.Text, fuel, dist, false, totalDist, "ready","pack://application:,,,/src/pics/okIcon.png");
+            buses bs1 = new buses((DateTime)dpRegiDate.SelectedDate, (DateTime)dplmiDate.SelectedDate, txbid.Text, fuel, dist, false, totalDist, "ready","/src/pics/okIcon.png");
             bs1.UpdateDangerous();
             mainWindow1.BusPool.Add(bs1);
             mainWindow1.bsDisplay.Items.Refresh();
