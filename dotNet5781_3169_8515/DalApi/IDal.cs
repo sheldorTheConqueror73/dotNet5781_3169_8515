e5c7849496f2ce006a0bd1d9aa3bf5d83a43d9aa
sheldorTheConqueror73 <yonatan.y.tamir@gmail.com>
2020-12-21 13:51:41 +0200

    finifshed Idal

--- dotNet5781_3169_8515/DalApi/IDal.cs
+++ dotNet5781_3169_8515/DalApi/IDal.cs
@@ -13,14 +13,38 @@ namespace DalApi
         IEnumerable<Bus> GetAllBuses();
         IEnumerable<Bus> GetAllBusesBy(Predicate<Bus> predicate);
         Bus GetBus(string id);
-        void add(Bus bus));
-        void remove(string id);
-        void update(Bus bus);
-
+        void addBus(Bus bus);
+        void removeBus(string id);
+        void updateBus(Bus bus);
         #endregion
 
         #region busLine
-
+        IEnumerable<busLine> GetAllbusLines();
+        IEnumerable<busLine> GetAllbusLinesBy(Predicate<busLine> predicate);
+        busLine GetBusLine(string id);
+        void addLine(busLine line);
+        void removeLine(string id);
+        void updateLine(busLine line);
         #endregion
+
+        #region Station
+        IEnumerable<busStation> GetAllbusStations();
+        IEnumerable<busStation> GetAllbusStationsBy(Predicate<busStation> predicate);
+        busStation GetbusStation(string id);
+        void addLine(busStation station);
+        void removeStation(string id);
+        void updateStation(busStation station);
+        #endregion
+
+        #region LineStation
+        IEnumerable<busLineStation> GetAllbusLineStation();
+        IEnumerable<busLineStation> GetAllbusLineStationBy(Predicate<busLineStation> predicate);
+        busLineStation GetbusLineStation(string id);
+        void addLine(busLineStation line);
+        void removebusLineStation(string id);
+        void updatebusLineStation(busLineStation line);
+        #endregion
+
+
     }
 }
