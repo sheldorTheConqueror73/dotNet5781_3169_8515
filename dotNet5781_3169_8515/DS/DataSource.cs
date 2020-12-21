d347780a7f89030a91a6961b038b53ae689823a5
sheldorTheConqueror73 <yonatan.y.tamir@gmail.com>
2020-12-21 14:13:22 +0200

    saved progress

--- dotNet5781_3169_8515/DS/DataSource.cs
+++ dotNet5781_3169_8515/DS/DataSource.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,79 @@ using System.Threading.Tasks;
 
 namespace DS
 {
-    public class DataSource
+    public static class DataSource
     {
+        public static List<Bus> buses;
+        public static List<busStation> stations;
+        public static List<busLineStation> LineStations;
+        public static List<busLine> Lines;
+        static DataSource()
+        {
+            InitAllLists();
+
+        }
+
+        private static void InitAllLists()
+        {
+
+            for (int i = 0; i < 13; i++)
+            {
+                bool flag = true;
+                string id = "";
+                DateTime rd = randomDate();
+                while (flag)
+                {
+                    if (rd.Year < 2018)
+                        id = r.Next(1000000, 10000000).ToString();//make sure id format matches MD 
+                    else
+                        id = r.Next(10000000, 100000000).ToString();
+                    flag = false;
+                    foreach (var bus in buses)
+                        if (id == bus.id)
+                        {
+                            flag = true;
+                            break;
+                        }
+                }
+                DateTime lastM = randomDate(1);
+                buses.Add(new Bus(rd, lastM, id, r.Next(0, buses.FULL_TANK), r.Next(0, 20001), false, r.Next(0, 120000), "ready", new Timerclass(0) { TimeNow = "00:00:00" }, "/src/pics/okIcon.png"));
+            }
+            buses[0].LastMaintenance = new DateTime(DateTime.Now.Year - 1, DateTime.Now.Month, DateTime.Now.Day);
+            buses[0].Status = "dangerous";
+            buses[0].Dangerous = true;
+            buses[0].IconPath = "/src/pics/warningIcon.png";
+            buses[1].Distance = 19999;
+            buses[2].Fuel = 0;
+
+        }
+
+        private static DateTime randomDate(int mode = 0)
+        {
+            int month, day, year;
+            year = r.Next(1980, DateTime.Now.Year + 1);
+            if (year == DateTime.Now.Year)
+            {
+                month = r.Next(1, DateTime.Now.Month + 1);
+                if (month == DateTime.Now.Month)
+                    day = r.Next(1, DateTime.Now.Day + 1);
+                else
+                    day = r.Next(1, 32);
+            }
+            else
+            {
+                month = r.Next(1, 13);
+                day = r.Next(1, 32);
+            }
+            if (mode == 1)
+                year = DateTime.Now.Year;
+            try
+            {
+                return new DateTime(year, month, day);
+            }
+            catch (Exception e)
+            {
+                return randomDate(mode);
+            }
+        }
     }
 }
--- dotNet5781_3169_8515/DalObject/DLObject.cs
+++ dotNet5781_3169_8515/DalObject/DLObject.cs
@@ -1,4 +1,5 @@
 ﻿using DalApi;
+using DO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,9 +15,9 @@ namespace DalObject
       static DLObject() { }
       DLObject() { } 
       public static DLObject Instance { get => instance; }
-        #endregion
+      #endregion
 
-
+     
 
 
 
@@ -28,7 +29,87 @@ namespace DalObject
             throw new NotImplementedException();
         }
 
+        public void addBus(Bus bus)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void addLine(busLine line)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void addLine(busStation station)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void addLine(busLineStation line)
+        {
+            throw new NotImplementedException();
+        }
+
         public object find()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Bus> GetAllBuses()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Bus> GetAllBusesBy(Predicate<Bus> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<busLine> GetAllbusLines()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<busLine> GetAllbusLinesBy(Predicate<busLine> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<busLineStation> GetAllbusLineStation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<busLineStation> GetAllbusLineStationBy(Predicate<busLineStation> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<busStation> GetAllbusStations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<busStation> GetAllbusStationsBy(Predicate<busStation> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Bus GetBus(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public busLine GetBusLine(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public busLineStation GetbusLineStation(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public busStation GetbusStation(string id)
         {
             throw new NotImplementedException();
         }
@@ -38,7 +119,47 @@ namespace DalObject
             throw new NotImplementedException();
         }
 
+        public void removeBus(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void removebusLineStation(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void removeLine(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void removeStation(string id)
+        {
+            throw new NotImplementedException();
+        }
+
         public void update()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateBus(Bus bus)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updatebusLineStation(busLineStation line)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateLine(busLine line)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateStation(busStation station)
         {
             throw new NotImplementedException();
         }
