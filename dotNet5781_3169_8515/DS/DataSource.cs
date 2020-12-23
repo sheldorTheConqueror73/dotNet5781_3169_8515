c8e9b7c1b40da95e88d196aa549dd164c9f3a345
sheldorTheConqueror73 <yonatan.y.tamir@gmail.com>
2020-12-23 15:43:09 +0200

    saved

--- dotNet5781_3169_8515/DS/DataSource.cs
+++ dotNet5781_3169_8515/DS/DataSource.cs
@@ -32,7 +32,7 @@ namespace DS
 
         private static void initUsers()
         {
-            users = new List<User> { new User { name = "Jack Smith" ,id="123456789", accessLevel=Clearance.Admin, password="aaa123"} };
+            users = new List<User> { new User { name = "Jack Smith" ,id="123456789", accessLevel=Clearance.Admin, password="aaa123"},new User { name = "Vladimir Putin", id = "123456788", accessLevel = Clearance.Operator, password = "polonium210" },new User { name = "C.M.O.T Dibbler", id = "123456787", accessLevel = Clearance.User, password = "bbb123" } };
         }
 
         private static void initBuses()
--- dotNet5781_3169_8515/PL/MainWindow.xaml.cs
+++ dotNet5781_3169_8515/PL/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,7 +21,7 @@ namespace PL
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+        IBL bL = BLFactory.GetBL();
         public MainWindow()
         {
             InitializeComponent();
