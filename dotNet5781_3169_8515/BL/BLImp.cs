7aeda57d3288a066935fe4d6ca1856d5afb4becd
itamaroh7 <itamaroh7@gmail.com>
2020-12-29 18:20:02 +0200

    add drivetime and distance between stations

--- dotNet5781_3169_8515/BL/BLImp.cs
+++ dotNet5781_3169_8515/BL/BLImp.cs
@@ -213,7 +213,9 @@ namespace BL
                 return (from folSta in folllowStation where folSta != null && folSta.enabled == true
                         from sta in stations
                         where sta!=null&&sta.enabled==true && folSta.firstStationid == id &&folSta.secondStationid==sta.id
-                        select DOtoBOConvertor<BO.busLineStation, DO.busLineStation>(sta)).ToList();
+                        let x=sta.Distance=folSta.distance
+                        let y=sta.DriveTime=folSta.driveTime
+                        select (DOtoBOConvertor<BO.busLineStation, DO.busLineStation>(sta))).ToList();
             return default;
         }
         #endregion
