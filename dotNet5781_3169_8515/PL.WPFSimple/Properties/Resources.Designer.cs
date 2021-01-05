d2139c81655785c1597ac97debdf6008cbd06bfb
sheldorTheConqueror73 <yonatan.y.tamir@gmail.com>
2021-01-05 22:18:36 +0200

    save

--- dotNet5781_3169_8515/PL.WPFSimple/Properties/Resources.Designer.cs
+++ dotNet5781_3169_8515/PL.WPFSimple/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace PL.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap bckg1 {
+            get {
+                object obj = ResourceManager.GetObject("bckg1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
