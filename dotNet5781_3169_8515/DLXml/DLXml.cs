dec2627b293299bf0a644a871b3c6b8126606cc7
sheldorTheConqueror73 <yonatan.y.tamir@gmail.com>
2021-01-13 22:46:04 +0200

    finished buses funxtion in DLxml hail kuku

--- dotNet5781_3169_8515/DLXml/DLXml.cs
+++ dotNet5781_3169_8515/DLXml/DLXml.cs
@@ -24,7 +24,15 @@ namespace DL
 
         public void updateBus(Bus bus)
         {
-            throw new NotImplementedException();
+            var result = from b1 in Utility.load(typeof(Bus)).Elements()
+                         select b1;
+            foreach (var element in result)
+                if (element.Attribute("id").Value == bus.id.ToString())
+                    element.ReplaceWith(bus.ToXml());
+            XElement root = new XElement("Buses");
+            foreach (var element in result)
+                root.Add(element);
+            Utility.save(root, typeof(Bus));
         }
         public IEnumerable<Bus> GetAllBuses()
         {
@@ -51,19 +59,33 @@ namespace DL
                    where element != null && obj.enabled == true && obj.id==id // change elemnt to obj
                    select obj).FirstOrDefault();
         }
+     
+
         public void removeBus(int id)
         {
-          
+            var bus = GetBus(id);
+            if (bus == null)
+                throw new NoSuchEntryException($"No Bus Matches ID number {id}");
+            bus.enabled = false;
+            updateBus(bus);
         }
 
         public void maintain(int id)
         {
-            throw new NotImplementedException();
+            var bus = GetBus(id);
+            bus.lastMaintenance = DateTime.Now;
+            bus.status = "ready";
+            bus.dangerous = false;
+            bus.fuel = Bus.FULL_TANK;
+            bus.distance = 0;
+            updateBus(bus);
         }
 
         public void refuel(int id)
         {
-            throw new NotImplementedException();
+            var bus = GetBus(id);
+            bus.fuel = Bus.FULL_TANK;
+            updateBus(bus);
         }
 
         #endregion
