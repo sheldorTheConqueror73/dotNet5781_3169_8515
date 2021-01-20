92bc71d95a9440e7835450bced12ea6e3aab2438
itamaroh7 <itamaroh7@gmail.com>
2021-01-20 11:30:31 +0200

    ibl commit

--- dotNet5781_3169_8515/BL/BLApi/IBL.cs
+++ dotNet5781_3169_8515/BL/BLApi/IBL.cs
@@ -65,7 +65,6 @@ namespace BLAPI
         BO.User GetUser(int id);
         void addUser(BO.User line);
         void removeUser(int id);
-        void updateUser(User user);
         string authenticate(string username, string password, out int id);
         int indexOfCbByAccessLevel(int id);
         void updateUser(User user);
